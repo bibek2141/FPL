b084c609ecf90f57aa0aa113b1c3428bc04e5414
Bibek Adhikari <bibekadhikari2141@gmail.com>
2024-08-21 19:54:48 -0700

    Resolved merge conflict in package-lock.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { TeamFixturesComponent } from './components/team-fixtures/team-fixtures.component';
 
-@NgModule({
-  declarations: [AppComponent, TeamFixturesComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent, TeamFixturesComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
