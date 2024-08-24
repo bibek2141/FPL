67ed958b560f702c86607b8ee65f9fcbe342d89d
Bibek Adhikari <bibekadhikari2141@gmail.com>
2024-08-23 18:05:16 -0700

    Render Issue 1.1

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 
 // Existing endpoint for FPL data
